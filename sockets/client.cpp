2f349e174a4ca173a6feee5db7b6c5c290e7cc27
lly <kirik0041@gmail.com>
2023-11-14 17:57:59 +0300

    qwe

--- sockets/client.cpp
+++ sockets/client.cpp
@@ -1,59 +1,55 @@
+#include <iostream>
 #include <cstdlib>
-#include <cstring>
-#include <strings.h>
-#include <stdio.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <unistd.h>
+#include <netdb.h>
 #include <arpa/inet.h>
+#include <cstring>
+#include <sys/types.h>
 
-int main(int argc, char* argv[]) {
-    char buffer[256];
+int main() {
+    int s = socket(AF_INET, SOCK_STREAM, 0);
 
-    int sockfd, portno, n;
-    struct sockaddr serv_addr;
-    struct hostent *server;
-
-    if (argc < 3) {
-        fprintf(stderr, "usage %s hostname port", argv[0]);
-        exit(0);
+    if (s == -1) {
+        perror("error socket\n");
+        exit(1);
     }
 
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in servAddr;
 
-    if (sockfd < 0)
-        perror("error opening socket");
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htons(54132);
+    std::cout << "ok1\n";
+    if (connect(s, (sockaddr *) &servAddr, sizeof(servAddr)) == -1) {
+        perror("connection failed\n");
+        exit(1);
+    }
+    std::cout << "ok2\n";
+    char msg[256];
 
-    server = gethostbyname(argv[1]);
+    while(true) {
+        printf("please enter a message: ");
+        memset(msg, 0, 256);
+        fgets(msg, 255, stdin);
 
-    if (server == NULL) {
-        fprintf(stderr, "error, no such host");
-        exit(0);
+        int n = write(s, msg, strlen(msg));
+        if (n == -1) {
+            perror("error writing to socket\n");
+            exit(1);
+        }
+
+        memset(msg, 0, 256);
+        n = read(s, msg, 255);
+
+        if (n == -1) {
+            perror("error read from socket\n");
+            exit(1);
+        }
+
+        printf("%s\n", msg);
     }
 
-    inet_pton(AF_INET, "127.0.0.1", &serv_addr);
-
-    if (connect(sockfd, &serv_addr, sizeof(serv_addr)) < 0)
-        perror("error connecting");
-
-    printf("please enter your message: ");
-    bzero(buffer, 256);
-    fgets(buffer, 255, stdin);
-
-    n = write(sockfd, buffer, strlen(buffer));
-
-    if (n < 0)
-        perror("error writing to socket");
-    bzero(buffer, 256);
-
-    n = read(sockfd, buffer, 255);
-    if (n < 0)
-        perror("error reading from socket");
-
-    printf("%s", buffer);
+    close(s);
 
     return 0;
 }
--- sockets/server.cpp
+++ sockets/server.cpp
@@ -1,76 +1,83 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
+#include <iostream>
+#include <cstdlib>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <sys/types.h>
 
-void dostuff(int); /* function prototype */
-void error(const char *msg)
-{
-    perror(msg);
-    exit(1);
-}
+int main() { 
+	int s = socket(AF_INET, SOCK_STREAM, 0);
 
-int main(int argc, char *argv[])
-{
-     int sockfd, newsockfd, portno, pid;
-     socklen_t clilen;
-     struct sockaddr_in serv_addr, cli_addr;
+	if (s == -1) {
+		perror("socket error\n");
+		exit(1);
+	}
 
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0)
-        error("ERROR opening socket");
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(54132);
+    //inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
+    
+    if (bind(s, (sockaddr *) &hint, sizeof(hint)) == -1) {
+        perror("bind error\n");
+        exit(1);
+    }
 
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
+    if(listen(s, SOMAXCONN) == -1) {
+        perror("listen error\n");
+        exit(1); 
+    }
 
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
+    sockaddr_in client;
+    socklen_t clientSize = sizeof(client);
+    char host[NI_MAXHOST];
+    char svc[NI_MAXSERV];
 
-     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-		error("ERROR on binding");
+    int clientSocket = accept(s, (sockaddr *)&client, &clientSize);
 
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
+    if (clientSocket == -1) {
+        perror("accept error\n");
+        exit(1);
+    }
 
-     while (1) {
-         newsockfd = accept(sockfd,
-               (struct sockaddr *) &cli_addr, &clilen);
-         if (newsockfd < 0)
-             error("ERROR on accept");
+    close(s);
 
-         pid = fork();
-         if (pid < 0)
-             error("ERROR on fork");
+    memset(host, 0, NI_MAXHOST);
+    memset(svc, 0, NI_MAXSERV);
 
-         if (pid == 0)  {
-             close(sockfd);
-             dostuff(newsockfd);
-             exit(0);
-         }
-         else close(newsockfd);
-     }
+    int result = getnameinfo((sockaddr*)&client, sizeof(client), 
+            host, NI_MAXHOST, svc, NI_MAXSERV, 0);
 
-     close(sockfd);
-     return 0;
-}
+    if (result) {
+        std::cout << host << " connected on " << svc << std::endl;
+    }
+    else {
+        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+        std::cout << host << " connected on " << svc << std::endl;
+    }
 
-void dostuff (int sock)
-{
-   int n;
-   char buffer[256];
+    char msg[4096];
+    std::cout << "server is waiting\n";
+    while(true) {
+        memset(msg, 0, 4096);
+        int byteRecv = recv(clientSocket, msg, 4096, 0);
 
-   bzero(buffer,256);
-   n = read(sock,buffer,255);
-   if (n < 0) error("ERROR reading from socket");
-   printf("Here is the message: %s\n",buffer);
-   n = write(sock,"I got your message",18);
-   if (n < 0) error("ERROR writing to socket");
+        if (byteRecv == -1) {
+            perror("connection issue\n");
+            break;
+        }
+
+        if (byteRecv == 0) {
+            std::cout << "The client disconnected\n";
+            break;
+        }
+
+        std::cout << "Received: " << std::string(msg, 0, byteRecv) << std::endl;
+
+        send(clientSocket, msg, byteRecv + 1, 0);
+    }
+
+    close(clientSocket);
 }
